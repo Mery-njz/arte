a4a3beca2890393de43687337637f11096196134
Mery-njz <m.agostinho@aluno.ifsp.edu.br>
2026-06-16 21:12:56 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -19,7 +19,7 @@ const etapas = [
     pergunta: "Uma figura aparece diante de você. O que faz?",
     respostas: [
       { texto: "Grita por ajuda", final: "Final ruim 1", fundo: "grito.jpg" },
-      { texto: "Tenta conversar", final: "Final neutro", fundo: "conversa.jpg" },
+      { texto: "Tenta conversar", final: "Final ???", fundo: "conversa.jpg" },
       { texto: "Ataca a figura", final: "Final bom", fundo: "ataque.jpg" }
     ]
   }
